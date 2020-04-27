30a3fa66685784aaba17270961f767bda815a5c2
basic-maaan <tnqkrtnqkr7756@gmail.com>
2020-04-27 15:32:42 +0900

    구글 맵 api 사용 현재 위치 표시

--- app/src/main/java/com/jbnu/project1/MainActivity.java
+++ app/src/main/java/com/jbnu/project1/MainActivity.java
@@ -3,11 +3,18 @@ package com.jbnu.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 
 import com.jbnu.project1.ui.login.LoginActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button belt = (Button)findViewById(R.id.belt);
         belt.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //편의 시설 위치 클릭시 활성화
-
+                Intent intent = new Intent(getApplicationContext(), Map.class);
+                startActivity(intent);
             }
         });
 
