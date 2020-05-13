40a1f33ee7c3ec69adc71cce53865adb9ae7c1d7
jimam <jiyum4395@gmail.com>
2020-05-13 16:26:31 +0900

    벨트화면

--- app/src/main/java/com/jbnu/project1/RegisterActivity.java
+++ app/src/main/java/com/jbnu/project1/RegisterActivity.java
@@ -32,6 +32,9 @@ public class RegisterActivity extends AppCompatActivity {
     private Button btn_register,validateButton;
     private AlertDialog dialog;
     private boolean validate=false;
+    Button b;
+    EditText et;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +46,29 @@ public class RegisterActivity extends AppCompatActivity {
         et_name=findViewById(R.id.et_name);
         et_num=findViewById(R.id.et_num);
         validateButton=findViewById(R.id.validateButton);
+
+        b = (Button) findViewById(R.id.validateButton);
+        et = (EditText) findViewById(R.id.et_id);
+
+        b.setEnabled(false); // set button disable initially
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                if (s.toString().equals("")) {
+                    b.setEnabled(false);
+                } else {
+                    b.setEnabled(true);
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
         et_passck.addTextChangedListener(new TextWatcher() {
             @Override
@@ -66,7 +92,6 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
-
 
 
         validateButton.setOnClickListener(new View.OnClickListener() {
@@ -110,6 +135,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
 
+
         btn_register=findViewById(R.id.btn_register);
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -118,11 +144,22 @@ public class RegisterActivity extends AppCompatActivity {
                 String userPass=et_pass.getText().toString();
                 String userName=et_name.getText().toString();
                 String userNum=et_num.getText().toString();
+                Button button = (Button) findViewById(R.id.validateButton);
+                String yourText = (String)button.getText();
 
 
                 if(userID.equals("")||userPass.equals("")||userName.equals("")||et_num.getText().toString().equals("") ){
                     AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
                     dialog = builder.setMessage("빈칸을 채워주세요")
+                            .setNegativeButton("OK", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+                if(yourText.equals("중복확인"))
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("아이디 중복확인을 해주세요")
                             .setNegativeButton("OK", null)
                             .create();
                     dialog.show();
@@ -136,27 +173,39 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
+                String a = et_pass.getText().toString();
+                // 대소문자 구분 숫자 특수문자  조합 8~ 15 자리
+                String pwPattern = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,15}$";
+                Boolean tt = Pattern.matches(pwPattern,a);
+                if( tt != true){
+                    Toast.makeText(RegisterActivity.this, "비밀번호 형식을 지켜주세요", Toast.LENGTH_SHORT).show();
+                    et_pass.setText("");
+                    et_passck.setText("");
+                    et_pass.requestFocus();
+                    return;
+                }
 
-                Response.Listener<String> responseListener=
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                try{
-                                    JSONObject jsonObject = new JSONObject(response);
-                                    boolean success = jsonObject.getBoolean("success");
-                                    if(success){
-                                        Toast.makeText(getApplicationContext(), "회원 등록에 성공",Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent( RegisterActivity.this, LoginActivity.class);
-                                        startActivity(intent);
-                                    }else{
-                                        Toast.makeText(getApplicationContext(), "회원 등록에 실패",Toast.LENGTH_SHORT).show();
-                                        return;
+
+                    Response.Listener<String> responseListener =
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    try {
+                                        JSONObject jsonObject = new JSONObject(response);
+                                        boolean success = jsonObject.getBoolean("success");
+                                        if (success) {
+                                            Toast.makeText(getApplicationContext(), "회원 등록에 성공", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                            startActivity(intent);
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "회원 등록에 실패", Toast.LENGTH_SHORT).show();
+                                            return;
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
                                     }
-                                } catch(JSONException e){
-                                    e.printStackTrace();
                                 }
-                            }
-                        };
+                            };
 
                 RegisterRequest registerRequest=new RegisterRequest(userID,userPass,userName,userNum,responseListener);
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
